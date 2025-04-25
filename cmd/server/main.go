76af63c3566010eae95dc6869ff607f2d74c1049
Alex <alex@thebarometer.co>
2025-04-25 13:31:28 -0700

    3.1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "fmt"
+    `github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub`
+    `github.com/bootdotdev/learn-pub-sub-starter/internal/routing`
     amqp "github.com/rabbitmq/amqp091-go"
     `log`
     `os`
@@ -18,6 +20,16 @@ func main() {
     }
     defer connection.Close()
     fmt.Println("Connected to AMQP server")
+    
+    channel, err := connection.Channel()
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    if err := pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true}); err != nil {
+        log.Fatal(err)
+    }
+    
     sigs := make(chan os.Signal, 1)
     signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
     <-sigs
